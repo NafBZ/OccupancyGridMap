ca33bbefabe762eaea6323464358b75c452d12e2
Nafees Zaman <zamannafees@gmail.com>
2022-03-26 15:33:18 +0100

    bresenham modified

--- src/utils_lib/bresenham.py
+++ src/utils_lib/bresenham.py
@@ -1,31 +1,94 @@
 #!/usr/bin/python3
 
-def bresenham(x0, y0, x1, y1):
-    """Yield integer coordinates on the line from (x0, y0) to (x1, y1).
-    Input coordinates should be integers.
-    The result will contain both the start and the end point.
-    """
-    dx = x1 - x0
-    dy = y1 - y0
+# def bresenham(x0, y0, x1, y1):
+#     """Yield integer coordinates on the line from (x0, y0) to (x1, y1).
+#     Input coordinates should be integers.
+#     The result will contain both the start and the end point.
+#     """
+#     dx = x1 - x0
+#     dy = y1 - y0
 
-    xsign = 1 if dx > 0 else -1
-    ysign = 1 if dy > 0 else -1
+#     xsign = 1 if dx > 0 else -1
+#     ysign = 1 if dy > 0 else -1
 
-    dx = abs(dx)
-    dy = abs(dy)
+#     dx = abs(dx)
+#     dy = abs(dy)
 
-    if dx > dy:
-        xx, xy, yx, yy = xsign, 0, 0, ysign
+#     if dx > dy:
+#         xx, xy, yx, yy = xsign, 0, 0, ysign
+#     else:
+#         dx, dy = dy, dx
+#         xx, xy, yx, yy = 0, ysign, xsign, 0
+
+#     D = 2*dy - dx
+#     y = 0
+
+#     for x in range(dx + 1):
+#         yield x0 + x*xx + y*yx, y0 + x*xy + y*yy
+#         if D >= 0:
+#             y += 1
+#             D -= 2*dx
+#         D += 2*dy
+
+
+#!/usr/bin/python3
+
+def line_high(start, end, width, points):
+    dx = end[0]-start[0]
+    dy = end[1]-start[1]
+    xi = width
+    if dx < 0:
+        xi = -width
+        dx = -dx
+    D = (2*dx) - dy
+    x = start[0]
+
+    for y in range(start[1], end[1]+width):
+        points.append([x, y])
+        if D > 0:
+            x = x + xi
+            D = D + (2*(dx-dy))
+        else:
+            D = D + (2*dx)
+    return points
+
+
+def line_low(start, end, width, points):
+    dx = end[0]-start[0]
+    dy = end[1]-start[1]
+    yi = width
+    if dy < 0:
+        yi = -width
+        dy = -dy
+    D = (2*dy) - dy
+    y = start[1]
+
+    for x in range(start[0], end[0]+width):
+        points.append([x, y])
+        if D > 0:
+            y = y + yi
+            D = D + (2*(dy-dx))
+        else:
+            D = D + (2*dy)
+    return points
+
+
+def bresenham_line(start, end, width=1):
+    # start:  2D lists
+    # end:    2D lists
+    # width:  scalar with resolution of grid map
+    # points: list of 2D points from start to end
+    points = []
+    if abs(end[1]-start[1]) < abs(end[0]-start[0]):
+
+        if start[0] > end[0]:
+            line_low(end, start, width, points)
+        else:
+            line_low(start, end, width, points)
     else:
-        dx, dy = dy, dx
-        xx, xy, yx, yy = 0, ysign, xsign, 0
+        if start[1] > end[1]:
+            line_high(end, width, start)
+        else:
+            line_high(start, width, end)
 
-    D = 2*dy - dx
-    y = 0
-
-    for x in range(dx + 1):
-        yield x0 + x*xx + y*yx, y0 + x*xy + y*yy
-        if D >= 0:
-            y += 1
-            D -= 2*dx
-        D += 2*dy
+    return points
--- src/utils_lib/gridmap.py
+++ src/utils_lib/gridmap.py
@@ -3,70 +3,50 @@
 import numpy as np
 from bresenham import *
 
-# Defining the functions to convert from Odds to log odds and viceversa
 
-
-def l2p(l):
-    return 1-(1/(1+np.exp(l)))
-
-
-def p2l(p):
-    return np.log(p/(1-p))
-
-
-model_p_occ = 0.75
-model_p_free = 0.45
-model_p_prior = 0.5
-
-model_l_occ = p2l(model_p_occ)
-model_l_free = p2l(model_p_free)
-model_l_prior = p2l(model_p_prior)
+def log_ods(p): return np.log(p/(1-p))
 
 
 class GridMap:
 
-    # Threshold values for the Log odss
     LMAX = 6.91
     LMIN = -6.91
-    # center = center position (x,y) of the grid with respect to the fixed_frame
-    # map_width = size of grid map in meters (width x width)
-    # cell_size = size of each cell in meters (cell_size x cell_size )
 
     def __init__(self, center, cell_size=0.1, map_width=30):
-        self.map_width = np.array([map_width, map_width])  # meters
-        self.cell_size = cell_size  # meters
+        self.map_width = np.array([map_width, map_width])
+        self.cell_size = cell_size
         self.grid = np.zeros(
             (int(map_width/cell_size), int(map_width/cell_size)))
         self.origin = np.array(center) - self.map_width/2
-        map_rows = int(map_width/cell_size)
-        map_cols = map_rows
 
     def get_map(self):
         return self.grid
 
     def update_cell(self, position, p):
 
-        for i in range(len(position)-2):
+        for i in range(len(position)-1):
             self.grid[position[i][1]][position[i][0]] = np.clip(
-                self.grid[position[i][1]][position[i][0]] + p2l(1-p), self.LMIN, self.LMAX)
+                self.grid[position[i][1]][position[i][0]] + log_ods(1-p), self.LMIN, self.LMAX)
 
         self.grid[position[-1][1]][position[-1][0]] = np.clip(
-            self.grid[position[-1][1]][position[-1][0]] + p2l(p), self.LMIN, self.LMAX)
+            self.grid[position[-1][1]][position[-1][0]] + log_ods(p), self.LMIN, self.LMAX)
 
     def add_ray(self, start, angle, range, p):
 
-        # Extracting start position of the ray
-        x0 = start[0]
-        y0 = start[1]
+        # Starting Postions
+        x_not = start[0]
+        y_not = start[1]
 
-        # Computing the end position of the ray
+        # Final Positions
         x1 = start[0] + range*np.cos(angle)
         y1 = start[1] + range*np.sin(angle)
 
         # Converting the start [i0,j0] and end [i1,j1] positions in cells
-        i0, j0 = self.position_to_cell([x0, y0])
-        i1, j1 = self.position_to_cell([x1, y1])
-        points = list(bresenham(int(i0), int(j0), int(i1), int(j1)))
+        start_x, start_y = self.position_to_cell([x_not, y_not])
+        end_x, end_y = self.position_to_cell([x1, y1])
+        bres_start = [int(start_x), int(start_y)]
+        bres_end = [int(end_x), int(end_y)]
+        points = list(bresenham_line(bres_start, bres_end, 1))
         self.update_cell(points, p)
 
         return self.grid
@@ -75,7 +55,7 @@ class GridMap:
         # Transform a position with respecto to the fixed_frame to a
         # cell index (rows and cols in the grid map)
 
-        i = ((position[0]-self.origin[0])/self.cell_size)
-        j = ((position[1]-self.origin[1])/self.cell_size)
+        row_position = ((position[0]-self.origin[0])/self.cell_size)
+        col_position = ((position[1]-self.origin[1])/self.cell_size)
 
-        return i, j
+        return row_position, col_position
